4b1834aa8d714c6cecb08a6a1f6785a6682c266e
javohir1606 <javohirallayorov1606@gmail.com>
2024-08-08 18:23:33 +0500

    main

---
+++ main.js
@@ -0,0 +1,78 @@
+// function uchburchak(a, b, c) {
+//   if (a == b && b == c && a == c) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// console.log(uchburchak(12, 12, 12));
+
+
+// function uchburchak(a, b, c) {
+//   if (a !== b && b !== c && a !== c) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// console.log(uchburchak(12, 13, 14));
+
+
+// function num(a, b, c){
+//   if(a == b && b == c){
+//     return true;
+//   }
+//   return false;
+// }
+
+// // console.log(12, 12, 12);
+
+// function son (a, b, c){
+//   if(a < b && b < c){
+//     return a+b+c;    
+//   }else if (a == c && (a > b && c > b || a < b && c < b)){
+//    return b;
+//   }else if(a == b && b == c){
+//     return 0;
+//   }
+// }
+
+// console.log(num(3, 7, 3));
+
+
+
+// function son(a, b, c){
+//   if(a < b && b < c){
+//     return true;
+//   }else if(a > b && b > c){
+//     return true;
+//   }else if (a != b && b != c){
+//     return false;
+//   }
+// }
+
+// console.log(son(1, 2, 3));
+
+
+// let str = "lorem" // lor
+// let res = "";
+// for (i = 0; i < str.length -2; i++){
+//   res += str[i];
+// }
+// console.log(res);
+
+
+let str = "oko" // aka
+let res = "";
+for (i = 0; i < str.length; i++){
+  res += str[i];
+}
+console.log(res);
+
+
+
+
+
+// Uygi-vazifa
+// 1 - misol
+
